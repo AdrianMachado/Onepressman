6889529cef40464f960b43a0628d0944ff414058
Dendrimer <franzjulius@gmail.com>
2016-01-23 09:55:52 -0500

    Updated build dependencies, added listeners
    Updated grade build dependencies to include pebblekit, and created a
    package for listeners

---
+++ app/src/main/java/amcd/opm/listeners/PebbleListener.java
@@ -0,0 +1,15 @@
+package amcd.opm.listeners;
+
+import com.getpebble.android.kit.PebbleKit;
+import com.getpebble.android.kit.PebbleKit.PebbleDataReceiver;
+import com.getpebble.android.kit.util.PebbleDictionary;
+
+
+/**
+ * Created by franzchen on 2016-01-23.
+ */
+public class PebbleListener {
+
+    private PebbleDataReceiver receiver;
+
+}
