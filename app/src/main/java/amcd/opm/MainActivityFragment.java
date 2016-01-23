ee9abcb35648e3eadac5319cac9c1380bba5ea65
Dendrimer <franzjulius@gmail.com>
2016-01-23 15:03:22 -0500

    Synced

--- app/src/main/java/amcd/opm/MainActivityFragment.java
+++ app/src/main/java/amcd/opm/MainActivityFragment.java
@@ -1,10 +1,12 @@
 package amcd.opm;
 
-import android.support.v4.app.Fragment;
+import android.app.FragmentManager;
+import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,8 +19,22 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
+
+    @Override
+    public void onDestroyView(){
+        super.onDestroyView();
+
+    }
+
+
+
+
+
+
 
 
 
